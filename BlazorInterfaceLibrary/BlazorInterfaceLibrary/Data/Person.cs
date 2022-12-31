86197f0863f75e39c4f86650cb408fff9cdfbfc5
MJ <mjn220@gmail.com>
2023-01-01 00:34:50 +0330

    Upload the project
    Upload whole project to repo as the initial push

---
+++ BlazorInterfaceLibrary/BlazorInterfaceLibrary/Data/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorInterfaceLibrary.Data
+{
+    public class Person
+    {
+        public string Date { get; set; }
+        public string Name { get; set; }
+        public string Postcode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
