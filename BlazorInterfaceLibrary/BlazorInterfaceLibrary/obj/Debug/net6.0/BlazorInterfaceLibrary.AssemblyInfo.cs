611b3404d5327f66b75cd7ec083080a8b6c296d3
MJ <mjn220@gmail.com>
2025-02-15 03:52:50 +0000

    Revert "Making the folders simple"
    This reverts commit e17e5f40ea5570e25f6dbb4c311ad029b063986b.

--- BlazorInterfaceLibrary/BlazorInterfaceLibrary/obj/Debug/net6.0/BlazorInterfaceLibrary.AssemblyInfo.cs
+++ BlazorInterfaceLibrary/BlazorInterfaceLibrary/obj/Debug/net6.0/BlazorInterfaceLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorInterfaceLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23859b75e8d4ef2ae64c0c940be9b608c1169d2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorInterfaceLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorInterfaceLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
