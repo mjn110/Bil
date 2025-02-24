5ad9f4df71a89fd7f9310120fbd4d89cfc99d9fb
MJ <mjn220@gmail.com>
2025-02-24 02:17:34 +0000

    Download functionality added to components
    In this commit the download functionality of download button have been implemented and now components are downloadable.

---
+++ BlazorInterfaceLibrary/wwwroot/js/download.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = (fileName, mimeType, base64) => {
+    const link = document.createElement("a");
+    link.href = "data:" + mimeType + ";base64," + base64;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
